665e705a838b12fe15b9fcfa3513f0f8ec757063
Ajinkya Chandrakant Bobade <ajinkyabobade93@gmail.com>
2018-12-20 22:37:58 +0530

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,227 @@
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.metrics import average_precision_score
+from xgboost.sklearn import XGBClassifier
+from xgboost import plot_importance, to_graphviz
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#####################################################################
+#                   Import data and print it
+#####################################################################
+
+df = pd.read_csv('PS_20174392719_1491204439457_log.csv')
+df = df.rename(columns={'oldbalanceOrg':'oldBalanceOrig', 'newbalanceOrig':'newBalanceOrig', \
+                        'oldbalanceDest':'oldBalanceDest', 'newbalanceDest':'newBalanceDest'})
+
+print(df.head())
+
+#####################################################################
+#                   Check for missing values in DataFrame
+#####################################################################
+
+print(df.isnull().values.any())
+
+#####################################################################
+#                   Check for Fraudent Transfers
+#####################################################################
+
+print('\n The types of fraudulent transactions are {}'.format(\
+list(df.loc[df.isFraud == 1].type.drop_duplicates().values))) # only 'CASH_OUT'
+                                                             # & 'TRANSFER'
+
+dfFraudTransfer = df.loc[(df.isFraud == 1) & (df.type == 'TRANSFER')]
+dfFraudCashout = df.loc[(df.isFraud == 1) & (df.type == 'CASH_OUT')]
+
+print ('\n The number of fraudulent TRANSFERs = {}'.\
+       format(len(dfFraudTransfer))) # 4097
+
+print ('\n The number of fraudulent CASH_OUTs = {}'.\
+       format(len(dfFraudCashout))) # 4116
+
+
+# It turns out that the origin of isFlaggedFraud is unclear.
+# isFlaggedFraud is set in a  meaningless way, we can treat this feature as insignificant and discard it.
+
+#####################################################################
+#     Checking if nameOrig and nameDest features are of any use
+#####################################################################
+
+
+print('\nAre there any merchants among originator accounts for CASH_IN \
+transactions? {}'.format(\
+(df.loc[df.type == 'CASH_IN'].nameOrig.str.contains('M')).any())) # False
+
+print('\nAre there any merchants among destination accounts for CASH_OUT \
+transactions? {}'.format(\
+(df.loc[df.type == 'CASH_OUT'].nameDest.str.contains('M')).any())) # False
+
+print('\nAre there merchants among any originator accounts? {}'.format(\
+      df.nameOrig.str.contains('M').any())) # False
+
+print('\nAre there any transactions having merchants among destination accounts\
+ other than the PAYMENT type? {}'.format(\
+(df.loc[df.nameDest.str.contains('M')].type != 'PAYMENT').any())) # False
+
+#nameOrig and nameDest features donot encode merchant accounts, we drop these features from the data since they are meaningless.
+
+print('\nWithin fraudulent transactions, are there destinations for TRANSFERS \
+that are also originators for CASH_OUTs? {}'.format(\
+(dfFraudTransfer.nameDest.isin(dfFraudCashout.nameOrig)).any())) # False
+dfNotFraud = df.loc[df.isFraud == 0]
+
+print('\nFraudulent TRANSFERs whose destination accounts are originators of \
+genuine CASH_OUTs: \n\n{}'.format(dfFraudTransfer.loc[dfFraudTransfer.nameDest.\
+isin(dfNotFraud.loc[dfNotFraud.type == 'CASH_OUT'].nameOrig.drop_duplicates())]))
+
+print('\nFraudulent TRANSFER to C423543548 occured at step = 486 whereas \
+genuine CASH_OUT from this account occured earlier at step = {}'.format(\
+dfNotFraud.loc[(dfNotFraud.type == 'CASH_OUT') & (dfNotFraud.nameOrig == \
+                           'C423543548')].step.values)) # 185
+
+print('#####################################################################')
+
+#####################################################################
+#     From the data analysis above
+#     we know that fraud only occurs in 'TRANSFER's and 'CASH_OUT's.
+#     So we assemble only the corresponding data in X for analysis.
+#####################################################################
+
+
+X = df.loc[(df.type == 'TRANSFER') | (df.type == 'CASH_OUT')]
+
+randomState = 5
+np.random.seed(randomState)
+
+#X = X.loc[np.random.choice(X.index, 100000, replace = False)]
+
+Y = X['isFraud']
+del X['isFraud']
+
+# Eliminate columns shown to be irrelevant for analysis in the EDA
+X = X.drop(['nameOrig', 'nameDest', 'isFlaggedFraud'], axis = 1)
+
+# Binary-encoding of labelled data in 'type'
+X.loc[X.type == 'TRANSFER', 'type'] = 0
+X.loc[X.type == 'CASH_OUT', 'type'] = 1
+X.type = X.type.astype(int) # convert dtype('O') to dtype(int)
+
+# Transactions in destination account where zero likely denotes a missing value, is much larger in
+# fraudulent (50%) compared to genuine transactions (0.06%).
+
+
+Xfraud = X.loc[Y == 1]
+XnonFraud = X.loc[Y == 0]
+print('\nThe fraction of fraudulent transactions with \'oldBalanceDest\' = \
+\'newBalanceDest\' = 0 although the transacted \'amount\' is non-zero is: {}'.\
+format(len(Xfraud.loc[(Xfraud.oldBalanceDest == 0) & \
+(Xfraud.newBalanceDest == 0) & (Xfraud.amount)]) / (1.0 * len(Xfraud))))
+
+print('\nThe fraction of genuine transactions with \'oldBalanceDest\' = \
+newBalanceDest\' = 0 although the transacted \'amount\' is non-zero is: {}'.\
+format(len(XnonFraud.loc[(XnonFraud.oldBalanceDest == 0) & \
+(XnonFraud.newBalanceDest == 0) & (XnonFraud.amount)]) / (1.0 * len(XnonFraud))))
+
+# The destination account balances being zero is a strong indicator of fraud.
+# Instead, below we replace the value of 0 with -1 which will be more useful to
+#  a suitable machine-learning (ML) algorithm detecting fraud.
+
+X.loc[(X.oldBalanceDest == 0) & (X.newBalanceDest == 0) & (X.amount != 0), \
+      ['oldBalanceDest', 'newBalanceDest']] = - 1
+
+#  instead of imputing a numerical value we replace the value of 0 with a null value.
+
+X.loc[(X.oldBalanceOrig == 0) & (X.newBalanceOrig == 0) & (X.amount != 0), \
+      ['oldBalanceOrig', 'newBalanceOrig']] = np.nan
+
+
+#   Feature-engineering
+
+X['errorBalanceOrig'] = X.newBalanceOrig + X.amount - X.oldBalanceOrig
+X['errorBalanceDest'] = X.oldBalanceDest + X.amount - X.newBalanceDest
+
+# Machine Learning
+# Split the data into training and test sets in a 80:20 ratio
+
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size = 0.2, \
+                                                random_state = randomState)
+
+# Long computation in this cell (~1.8 minutes)
+weights = (Y == 0).sum() / (1.0 * (Y == 1).sum())
+clf = XGBClassifier(max_depth = 3, scale_pos_weight = weights, \
+                n_jobs = 4)
+
+
+print('skew = {}'.format( len(Xfraud) / float(len(X)) ))
+
+#To deal with the large skew in the data, we chose an appropriate metric
+# and used an ML algorithm based on an ensemble of decision trees which
+# works best with strongly imbalanced classes.
+
+
+probabilities = clf.fit(trainX, trainY).predict_proba(testX)
+print('AUPRC = {}'.format(average_precision_score(testY, \
+                                              probabilities[:, 1])))
+
+
+# Figuring out the important features of this model
+
+fig = plt.figure(figsize=(14, 9))
+ax = fig.add_subplot(111)
+
+colours = plt.cm.Set1(np.linspace(0, 1, 9))
+
+ax = plot_importance(clf, height=1, color=colours, grid=False, \
+                     show_values=False, importance_type='cover', ax=ax);
+for axis in ['top', 'bottom', 'left', 'right']:
+    ax.spines[axis].set_linewidth(2)
+
+ax.set_xlabel('importance score', size=16);
+ax.set_ylabel('features', size=16);
+ax.set_yticklabels(ax.get_yticklabels(), size=12);
+ax.set_title('Ordering of features by importance to the model learnt', size=20)
+
+plt.show()
+
+
+
+#to_graphviz(clf)
+
+trainSizes, trainScores, crossValScores = learning_curve(\
+XGBClassifier(max_depth = 3, scale_pos_weight = weights, n_jobs = 4), trainX,\
+                                         trainY, scoring = 'average_precision')
+
+trainScoresMean = np.mean(trainScores, axis=1)
+trainScoresStd = np.std(trainScores, axis=1)
+crossValScoresMean = np.mean(crossValScores, axis=1)
+crossValScoresStd = np.std(crossValScores, axis=1)
+
+colours = plt.cm.tab10(np.linspace(0, 1, 9))
+
+fig = plt.figure(figsize = (14, 9))
+plt.fill_between(trainSizes, trainScoresMean - trainScoresStd,
+    trainScoresMean + trainScoresStd, alpha=0.1, color=colours[0])
+plt.fill_between(trainSizes, crossValScoresMean - crossValScoresStd,
+    crossValScoresMean + crossValScoresStd, alpha=0.1, color=colours[1])
+plt.plot(trainSizes, trainScores.mean(axis = 1), 'o-', label = 'train', \
+         color = colours[0])
+plt.plot(trainSizes, crossValScores.mean(axis = 1), 'o-', label = 'cross-val', \
+         color = colours[1])
+
+ax = plt.gca()
+for axis in ['top','bottom','left','right']:
+    ax.spines[axis].set_linewidth(2)
+
+handles, labels = ax.get_legend_handles_labels()
+plt.legend(handles, ['train', 'cross-val'], bbox_to_anchor=(0.8, 0.15), \
+               loc=2, borderaxespad=0, fontsize = 16);
+plt.xlabel('training set size', size = 16)
+plt.ylabel('AUPRC', size = 16)
+plt.title('Learning curves indicate slightly underfit model', size = 20)
+
